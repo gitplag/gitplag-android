9a00a5ba0479e9d326b5841d9dad49a34b99b26d
Nikita Stepochkin <nikstepbox@gmail.com>
2020-07-04 19:04:54 +0300

    feat: network module

--- app/src/main/java/io/gitplag/gitplag/android/client/GitplagClient.kt
+++ app/src/main/java/io/gitplag/gitplag/android/client/GitplagClient.kt
@@ -1,11 +1,7 @@
 package io.gitplag.gitplag.android.client
 
-import io.gitplag.gitplag.android.BuildConfig.GITPLAG_SERVER_URL
 import io.gitplag.gitplag.android.model.Repository
 import io.reactivex.Observable
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -17,12 +13,4 @@ interface GitplagClient {
     @GET("/api/repositories/{id}")
     fun getRepository(@Path("id") id: Long): Observable<Repository>
 
-    companion object Factory {
-        fun create(): GitplagClient = Retrofit.Builder()
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl(GITPLAG_SERVER_URL)
-            .build()
-            .create(GitplagClient::class.java)
-    }
 }
--- app/src/main/java/io/gitplag/gitplag/android/di/NetworkModule.kt
+++ app/src/main/java/io/gitplag/gitplag/android/di/NetworkModule.kt
@@ -3,13 +3,47 @@ package io.gitplag.gitplag.android.di
 import dagger.Module
 import dagger.Provides
 import dagger.Reusable
+import io.gitplag.gitplag.android.BuildConfig
 import io.gitplag.gitplag.android.client.GitplagClient
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 @Module
 object NetworkModule {
 
     @Provides
     @Reusable
-    fun providePostApi(): GitplagClient = GitplagClient.create()
+    fun provideOKHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .readTimeout(120, TimeUnit.SECONDS)
+        .connectTimeout(120, TimeUnit.SECONDS)
+        .build()
+
+    @Provides
+    @Reusable
+    fun provideGSON(): GsonConverterFactory = GsonConverterFactory.create()
+
+    @Provides
+    @Reusable
+    fun provideRxJava2CallAdapterFactory(): RxJava2CallAdapterFactory = RxJava2CallAdapterFactory.create()
+
+    @Provides
+    @Reusable
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory,
+        rxJava2CallAdapterFactory: RxJava2CallAdapterFactory
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.GITPLAG_SERVER_URL)
+        .client(okHttpClient)
+        .addConverterFactory(gsonConverterFactory)
+        .addCallAdapterFactory(rxJava2CallAdapterFactory)
+        .build()
+
+    @Provides
+    @Reusable
+    fun provideGitplagClient(retrofit: Retrofit): GitplagClient = retrofit.create(GitplagClient::class.java)
 
 }
